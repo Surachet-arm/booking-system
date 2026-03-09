aa3fc31466522d00f1b0c8d360a344eec889464b
ชื่Surachet-arm <oarmking@gmail.com>
2026-03-09 21:25:04 +0700

    fix path3

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ dotenv.config();
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import { connectDB } from "./config/db.js";
 import auth from "./routes/auth.js";
@@ -21,14 +22,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/api/events", events);
 app.use("/api/bookings", bookings);
-
-app.use(
-  "/uploads",
-  express.static(path.join(process.cwd(), "backend", "uploads"))
-);
 
 app.listen(5000, () => console.log("Server running"));
