65f996a1ef4ad411abc05925974dca68185108b9
ชื่Surachet-arm <oarmking@gmail.com>
2026-03-09 21:38:15 +0700

    fix path api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,3 @@
-import dns from "dns";
-dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import path from "path";
